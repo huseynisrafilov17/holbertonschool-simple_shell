bf0cf0f15ddf9a7786c1fa416bdcb1a329a6988a
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-12-28 07:06:29 -0800

    Updated execute code and related things

--- execute.c
+++ execute.c
@@ -6,23 +6,26 @@
 
 void execute(char **args, char *filename)
 {
-	int status;
+	int *status = NULL, child_pid;
 	if (args[0])
 	{
-		if (fork() == 0)
+		child_pid = fork();
+		if (child_pid == -1)
+			perror("Error:");
+		else if (child_pid == 0)
 		{
 			if (execve(args[0], args, NULL) == -1)
 			{
 				perror("Error");
-				exit(status);
+				exit(127);
 			}
 		}
 		else
-			wait(&status);
+			wait(status);
 	}
 	else
 	{
-		dprintf(STDERR_FILENO, "./hsh: 1: %s: not found.\n", filename);
-		exit(status);
+		dprintf(STDERR_FILENO, "./hsh: 1: %s: not found\n", filename);
+		exit(127);
 	}
 }
