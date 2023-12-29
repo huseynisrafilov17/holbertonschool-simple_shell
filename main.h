60547db7b300ebafa8c821285b64c31b3a18de24
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-12-29 04:14:31 -0800

    fixed merging problems

--- main.h
+++ main.h
@@ -12,7 +12,7 @@ extern char **environ;
 int delim_counter(char *str, char *delim);
 char **token_parse(char *str, char *delim);
 char *check_file(char **path_arr, char *filepath, int *flag);
-int execute(char **args, char *filename, int *status);
+int execute(char **args, char *filename);
 void _printenv();
 
 #endif
