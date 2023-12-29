6ce51104ea23c846bf85df2b4d8938ed2a3f1191
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-12-29 04:12:25 -0800

    reset back

--- execute.c
+++ execute.c
@@ -1,1 +1,38 @@
+#include "main.h"
+#include <sys/wait.h>
+/**
+ * execute - executes a command.
+ * @args: arguments and command.
+ * @filename: original command written by user. We used it,
+ * because if file doesn't exist we return NULL to arg[0].
+ * Return: status.
+ */
+int execute(char **args, char *filename)
+{
+	int status = 0, child_pid;
 
+	if (strcmp(filename, "exit") == 0)
+		return(status);
+	if (args[0])
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+			perror("Error:");
+		else if (child_pid == 0)
+		{
+			if (execve(args[0], args, environ) == -1)
+			{
+				perror("Error");
+				exit(127);
+			}
+		}
+		else
+			wait(&status);
+	}
+	else
+	{
+		dprintf(STDERR_FILENO, "./hsh: 1: %s: not found\n", filename);
+		status = 127;
+	}
+	return (status);
+}
